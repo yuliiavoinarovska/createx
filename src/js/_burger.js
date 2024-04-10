9e986e3406fc2ee31e4b28f5277090b523a1e26e
Julia <uvojnarovskaa@gmail.com>
2024-04-10 19:24:13 +0300

    add js for mobile menu

--- src/js/_burger.js
+++ src/js/_burger.js
@@ -1,17 +1,41 @@
 window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.nav');
-  const menuItem = document.querySelectorAll('.nav__item');
-  const hamburger = document.querySelector('.burger');
+  const burger = document.querySelector('.burger');
+  const overlay = document.querySelector('.overlay');
+  const dropdownIcon = document.querySelector('.dropdown__icon');
+  const dropdownItem = document.querySelector('.dropdown');
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('burger--active');
-    menu.classList.toggle('nav--active');
+  const lockScroll = () => {
+    document.body.classList.add('lock');
+  };
+
+  const unlockScroll = () => {
+    document.body.classList.remove('lock');
+  };
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger--active');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('open');
+    lockScroll();
   });
 
-  menuItem.forEach((item) => {
-    item.addEventListener('click', () => {
-      hamburger.classList.toggle('burger--active');
-      menu.classList.toggle('nav--active');
+  overlay.addEventListener('click', () => {
+    menu.classList.remove('open');
+    overlay.classList.remove('open');
+    burger.classList.remove('burger--active');
+    unlockScroll();
+  });
+
+  const dropdownTriggers = document.querySelectorAll('.dropdown');
+
+  dropdownTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      const dropdownMenu = trigger.nextElementSibling;
+      dropdownMenu.classList.toggle('active');
+      dropdownIcon.classList.toggle('active');
+      dropdownItem.classList.toggle('active');
     });
   });
 });
