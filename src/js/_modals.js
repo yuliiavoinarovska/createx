9d4f77a66e4d4105d1cc7d1822c4a4087d04dd8f
Julia <uvojnarovskaa@gmail.com>
2024-04-11 11:09:14 +0300

    upd modal js

--- src/js/_modals.js
+++ src/js/_modals.js
@@ -2,28 +2,30 @@ const modalTrigger = document.querySelector('[data-modal]');
 const modal = document.querySelector('.modal');
 const modalCloseBtn = document.querySelector('[data-close]');
 
-modalTrigger.addEventListener('click', () => {
-  modal.classList.add('modal--show');
-  modal.classList.remove('modal--hide');
-  document.body.style.overflow = 'hidden';
-});
-
 function closeModal() {
   modal.classList.add('modal--hide');
   modal.classList.remove('modal--show');
   document.body.style.overflow = '';
 }
 
-modalCloseBtn.addEventListener('click', closeModal);
+if (modal) {
+  modalTrigger.addEventListener('click', () => {
+    modal.classList.add('modal--show');
+    modal.classList.remove('modal--hide');
+    document.body.style.overflow = 'hidden';
+  });
 
-modal.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    closeModal();
-  }
-});
+  modalCloseBtn.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === 'Escape') {
-    closeModal();
-  }
-});
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  });
+}
